6d7896b5bb11fb6edd3c621970aff71d9ffea914
iLexiconn <ilexiconn@gmail.com>
2015-11-13 20:08:39 +0100

    Removed redstone support

--- src/main/java/net/ilexiconn/showcase/server/block/BlockShowcase.java
+++ src/main/java/net/ilexiconn/showcase/server/block/BlockShowcase.java
@@ -2,14 +2,10 @@ package net.ilexiconn.showcase.server.block;
 
 import net.ilexiconn.showcase.Showcase;
 import net.ilexiconn.showcase.server.block.entity.BlockEntityShowcase;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyBool;
-import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
@@ -18,15 +14,12 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 public class BlockShowcase extends BlockContainer {
-    public static final PropertyBool POWERED = PropertyBool.create("powered");
-
     public BlockShowcase() {
         super(Material.rock);
         setUnlocalizedName("showcase");
         setCreativeTab(CreativeTabs.tabDecorations);
         setHardness(-1f);
         setResistance(-1f);
-        setDefaultState(blockState.getBaseState().withProperty(POWERED, false));
     }
 
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ) {
@@ -35,12 +28,6 @@ public class BlockShowcase extends BlockContainer {
             return true;
         } else {
             return false;
-        }
-    }
-
-    public void onNeighborBlockChange(World world, BlockPos pos, IBlockState state, Block neighborBlock) {
-        if (!world.isRemote) {
-            world.setBlockState(pos, state.withProperty(POWERED, world.isBlockPowered(pos)));
         }
     }
 
@@ -62,21 +49,5 @@ public class BlockShowcase extends BlockContainer {
 
     public TileEntity createNewTileEntity(World world, int meta) {
         return new BlockEntityShowcase();
-    }
-
-    public IBlockState getStateFromMeta(int meta) {
-        return getDefaultState().withProperty(POWERED, meta == 1);
-    }
-
-    public int getMetaFromState(IBlockState state) {
-        return (Boolean) state.getValue(POWERED) ? 1 : 0;
-    }
-
-    public BlockState createBlockState() {
-        return new BlockState(this, POWERED);
-    }
-
-    public IBlockState onBlockPlaced(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return getDefaultState().withProperty(POWERED, false);
     }
 }
