e0684e10a613c5940fbdcdc9c1cc0ce90c1b9774
iLexiconn <ilexiconn@gmail.com>
2015-11-16 17:17:20 +0100

    Released 0.1.1

--- src/main/java/net/ilexiconn/showcase/Showcase.java
+++ src/main/java/net/ilexiconn/showcase/Showcase.java
@@ -19,7 +19,7 @@ import net.ilexiconn.showcase.server.confg.ShowcaseConfig;
 import net.ilexiconn.showcase.server.message.MessageUpdate;
 import net.minecraft.block.Block;
 
-@Mod(modid = "showcase", name = "Showcase", version = Showcase.VERSION, dependencies = "required-after:Forge@[11.13.4.1558,99999.13.0.0);required-after:llibrary@[0.5.3,)")
+@Mod(modid = "showcase", name = "Showcase", version = Showcase.VERSION, dependencies = "required-after:llibrary@[0.5.4]")
 public class Showcase {
     @SidedProxy(serverSide = "net.ilexiconn.showcase.server.ServerProxy", clientSide = "net.ilexiconn.showcase.client.ClientProxy")
     public static ServerProxy proxy;
@@ -28,7 +28,7 @@ public class Showcase {
     public static LoggerHelper logger = new LoggerHelper("Showcase");
     public static SimpleNetworkWrapper networkWrapper;
 
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
 
     public static Block blockShowcase;
 
